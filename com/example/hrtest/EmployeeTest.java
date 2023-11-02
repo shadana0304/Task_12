50d80ffd2405df1a9fff6e6e9906dd208b7191d2
shadana0304 <saranya3606@gmail.com>
2023-11-02 15:19:42 +0530

    Task completed

---
+++ com/example/hrtest/EmployeeTest.java
@@ -0,0 +1,11 @@
+package com.example.hrtest;
+import java.util.Scanner;
+import com.example.hr.Employee;
+public class EmployeeTest {
+         public static void main(String[] args) {
+  			Employee em = new Employee();
+  			em.printName("saranya");
+  			em.printSalary(50000);
+  			
+         }
+}
